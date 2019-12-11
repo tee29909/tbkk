08e398df9f385bf75fe4096ebd3c586468a835d5
tee29909 <59160571@go.buu.ac.th>
2019-12-11 23:42:24 +0700

    add all

--- tbkk/Pages/listOTs/listEmpOT.cshtml.cs
+++ tbkk/Pages/listOTs/listEmpOT.cshtml.cs
@@ -18,11 +18,18 @@ namespace tbkk.Pages.listOTs
             _context = context;
         }
 
+        
         public IList<DetailOT> DetailOT { get;set; }
         public IList<OT> OT { get; set; }
         public Employee Employee { get; set; }
 
         public async Task OnGetAsync(int? id)
+        {
+            await OnLoad(id);
+
+        }
+
+        private async Task OnLoad(int? id)
         {
             Employee = await _context.Employee
               .Include(e => e.Company)
@@ -31,7 +38,7 @@ namespace tbkk.Pages.listOTs
               .Include(e => e.Location)
               .Include(e => e.Position).FirstOrDefaultAsync(m => m.EmployeeID == id);
             DetailOT = await _context.DetailOT
-               
+
                 .Include(d => d.Employee)
                 .Include(d => d.FoodSet)
                 .Include(d => d.OT)
@@ -40,9 +47,44 @@ namespace tbkk.Pages.listOTs
             DetailOT = DetailOT.Where(d => d.Status.Equals("Pending for approval")).ToList();
             OT = await _context.OT.ToListAsync();
             OT = OT.Where(o => o.TypStatus.Equals("Manage Car")).ToList();
+        }
+
+        public async Task<IActionResult> OnPostAddAllAsync(int id, int Did)
+        {
+            await OnLoad(id);
+            List<DetailOT> newDetailOTs = DetailOT.Where(n => n.OT_OTID == Did).ToList();
+            foreach (var item in newDetailOTs)
+            {
+                DetailOT newDetailOT = item;
+                newDetailOT.Status = "Allow";
+
+                _context.Attach(newDetailOT).State = EntityState.Modified;
+
+                try
+                {
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!DetailOTExists(newDetailOT.DetailOTID))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+            }
+
             
 
-           
+            return RedirectToPage("./../listOTs/listOT", new { id = Employee.EmployeeID });
+        }
+
+        private bool DetailOTExists(int id)
+        {
+            return _context.DetailOT.Any(e => e.DetailOTID == id);
         }
     }
 }
